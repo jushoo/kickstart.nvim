050c35c0105abbae0fefecc02f09fe041bd6f70e
jushoo <132144846+jushoo@users.noreply.github.com>
2024-02-07 22:55:47 +0800

    add autotags, emmet and show hidden files on neotree

--- lua/custom/plugins/tree.lua
+++ lua/custom/plugins/tree.lua
@@ -10,6 +10,13 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      }
+    }
   end,
 }
